021375c914b9f44fc0d344885df3ff9207ec8d16
Guillaume <gsylvestre@gmail.com>
2020-11-30 13:37:48 +0100

    récupère la liste des wish et les affiche

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +12,14 @@ class WishController extends AbstractController
     /**
      * @Route("/wishes", name="wish_list")
      */
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
+        //récupère les Wish publiés, du plus récent au plus ancien
+        $wishes = $wishRepository->findBy(['isPublished' => true], ['dateCreated' => 'DESC']);
+
         return $this->render('wish/list.html.twig', [
+            //les passe à Twig
+            "wishes" => $wishes
         ]);
     }
 
